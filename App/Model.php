38e455198b06ac5d70e8338563af24d615943040
паша <afoninn@i.ua>
2017-02-02 03:57:49 +0200

    + родительская модель

---
+++ App/Model.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: паша
+ * Date: 02.02.2017
+ * Time: 3:32
+ */
+
+namespace App;
+
+class Model
+{
+    const TABLE = '';
+    public static function findAll()
+    {
+        $db =new Db();
+
+        return $db->query(
+            'SELECT * FROM ' . static::TABLE,
+            static::class
+        );
+    }
+}
